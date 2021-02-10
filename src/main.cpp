a9ae7931119a32d62909e7e7150e60d799ebb380
Samson Gasparyan <samsongasparyan@gmail.com>
2021-02-10 15:57:23 +0100

    Starting to develop GPS lib

---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+#include <errno.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <zephyr.h>
+#include <arch/cpu.h>
+#include <sys/byteorder.h>
+#include <logging/log.h>
+#include <sys/util.h>
+
+#include <device.h>
+#include <init.h>
+#include <drivers/uart.h>
+
+#define LOG_MODULE_NAME hci_uart
+LOG_MODULE_REGISTER(LOG_MODULE_NAME);
+
+static const struct device *hci_uart_dev;
+static K_THREAD_STACK_DEFINE(tx_thread_stack, CONFIG_BT_HCI_TX_STACK_SIZE);
+static struct k_thread tx_thread_data;
+static K_FIFO_DEFINE(tx_queue);
+
+/* RX in terms of bluetooth communication */
+static K_FIFO_DEFINE(uart_tx_queue);
+
+#define H4_CMD 0x01
+#define H4_ACL 0x02
+#define H4_SCO 0x03
+#define H4_EVT 0x04
+
+/* Receiver states. */
+#define ST_IDLE 0	/* Waiting for packet type. */
+#define ST_HDR 1	/* Receiving packet header. */
+#define ST_PAYLOAD 2	/* Receiving packet payload. */
+#define ST_DISCARD 3	/* Dropping packet. */
